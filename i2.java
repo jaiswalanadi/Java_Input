a42e8aa0f756241c5352cb16b85bc8b2a4d44d94
Anadi Jaiswal <105920531+jaiswalanadi@users.noreply.github.com>
2023-02-03 19:04:43 +0530

    Add files via upload

---
+++ i2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class i2 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String first = scn.nextLine();
+        String last = scn.nextLine();
+        System.out.println(first + last);
+    }
+}
---
+++ i5.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class i5 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        while (scn.hasNextLine()) {
+            String a = scn.next();
+            String b = scn.next();
+            System.out.println(a + b);
+        }
+    }
+}
